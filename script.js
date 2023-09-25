9f464a8b5709268556ce93785d5755813f3a50ef
090631 <92621700+090631@users.noreply.github.com>
2023-09-26 02:47:44 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
+//coverflow
+document.addEventListener('DOMContentLoaded', function(event) {
     var swiper = new Swiper(".mySwiper", {
         effect: "coverflow",
         grabCursor: true,
         centeredSlides: true,
         slidesPerView: "auto",
+        keyboard: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
         coverflowEffect: {
           rotate: 0,
           stretch: 100,
@@ -14,46 +20,54 @@ document.addEventListener('DOMContentLoaded', function() {
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
+          dynamicBullets: true,
        },
     });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+//music-player
+document.addEventListener('DOMContentLoaded', function(event) {
+  let progress_bar = document.querySelector("input[type='range']");
+  let song = document.querySelector("audio");
+  let button_icon = document.querySelector("#button_icon");
+  let button = document.querySelector("#button");
+  //play pause
+  button.addEventListener("click", function(event){
+
+    if (button_icon.classList.contains("fa-play")){
+      song.play();
+      button_icon.classList.remove("fa-play");
+      button_icon.classList.add("fa-pause");
+    }else if (button_icon.classList.contains("fa-pause")){
+        song.pause();
+        button_icon.classList.remove("fa-pause");
+        button_icon.classList.add("fa-play");
+    }
+
+  });
+  //progress bar update
+  if(song.play()) {
+    setInterval(()=>{progress_bar.value = song.currentTime;}, 500);
+  }
+ //progress bar functionality
+  progress_bar.addEventListener("change", function(){
+    song.play();
+    song.currentTime = progress_bar.value;
+    button_icon.classList.remove("fa-play");
+    button_icon.classList.add("fa-pause");
+  });
+
+});
+
+//under works
+document.addEventListener('DOMContentLoaded', function(event) {
+
   let page = document.querySelector('p');
   let body = document.querySelector('body');
+
   page.addEventListener('click', function(event){
     body.style.background = 'url(https://th.bing.com/th/id/R.737c8b5516687272221ac78311f0ac5b?rik=w9HJeTNtJd6YMw&pid=ImgRaw&r=0)';
     body.style.backgroundSize = 'cover';
   })
 
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  let progress = document.querySelector("input");
-  let song = document.querySelector("audio");
-  let button = document.querySelector("#button");
-  let click = document.querySelector("#click");
-
-  click.addEventListener("click", function(event){
-    if (button.classList.contains("fa-play")){
-      song.play();
-      button.classList.remove("fa-play");
-      button.classList.add("fa-pause");
-    }else if (button.classList.contains("fa-pause")){
-        song.pause();
-        button.classList.remove("fa-pause");
-        button.classList.add("fa-play");
-    }
-  });
-  if(song.play()) {
-    setInterval(()=>{
-        progress.value = song.currentTime;
-    }, 500);
-  }
-  progress.addEventListener("change", function(){
-    song.play();
-    song.currentTime = progress.value;
-    button.classList.remove("fa-play");
-    button.classList.add("fa-pause");
-  });
 });
