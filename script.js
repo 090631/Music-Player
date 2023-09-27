d0454068744f75dacaaa293c1401403944bf2c09
090631 <92621700+090631@users.noreply.github.com>
2023-09-27 15:41:30 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,109 @@
-//coverflow
 document.addEventListener('DOMContentLoaded', function(event) {
-    var swiper = new Swiper(".mySwiper", {
-        effect: "coverflow",
-        grabCursor: true,
-        centeredSlides: true,
-        slidesPerView: "auto",
-        keyboard: true,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        coverflowEffect: {
-          rotate: 0,
-          stretch: 100,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-          dynamicBullets: true,
-       },
+
+  var swiper = new Swiper(".mySwiper", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      keyboard: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 100,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+     },
+  });
+
+});
+
+
+document.addEventListener('DOMContentLoaded', function(event) {
+
+  const bgArray = [
+    "url(https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80)",
+    "url(https://images.unsplash.com/photo-1517825738774-7de9363ef735?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1710&q=80)",
+    "url(https://images.unsplash.com/photo-1465628976988-fe43bda15798?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1772&q=80)"
+  ];
+
+  const progress_bar = document.querySelectorAll(`#progress_bar`);
+  const song = document.querySelectorAll(`#song`);
+  const button_icon = document.querySelectorAll(`#button_icon`);
+  const button = document.querySelectorAll(`#button`);
+  const next = document.querySelectorAll(`#nextS`);
+  const prev = document.querySelectorAll(`#prevS`);
+  const bg = document.querySelector(`body`);
+
+  for (let i = 0; i < 3; i++){
+    prev[i].addEventListener("click", function(event){
+      pause(i);
+      play(i-1);
+      change_bg(i-1);
     });
-});
 
-//music-player
-document.addEventListener('DOMContentLoaded', function(event) {
-  let progress_bar = document.querySelector("input[type='range']");
-  let song = document.querySelector("audio");
-  let button_icon = document.querySelector("#button_icon");
-  let button = document.querySelector("#button");
-  //play pause
-  button.addEventListener("click", function(event){
+    next[i].addEventListener("click", function(event){
+      pause(i);
+      play(i+1);
+      change_bg(i+1);
+    });
 
-    if (button_icon.classList.contains("fa-play")){
-      song.play();
-      button_icon.classList.remove("fa-play");
-      button_icon.classList.add("fa-pause");
-    }else if (button_icon.classList.contains("fa-pause")){
-        song.pause();
-        button_icon.classList.remove("fa-pause");
-        button_icon.classList.add("fa-play");
+    button[i].addEventListener("click", function(event){
+      play_pause(i);
+    });
+
+    progress_bar[i].addEventListener("change", function(){
+      progress_change(i);
+    });
+
+    if(song[i].play()) {
+      setInterval(()=>{progress_bar[i].value = song[i].currentTime;},
+      500);
+    };
+  };
+
+  function play_pause(num){
+    if (button_icon[num].classList.contains("fa-play")){
+      song[num].play();
+      button_icon[num].classList.remove("fa-play");
+      button_icon[num].classList.add("fa-pause");
+    }else if (button_icon[num].classList.contains("fa-pause")){
+        song[num].pause();
+        button_icon[num].classList.remove("fa-pause");
+        button_icon[num].classList.add("fa-play");
     }
+  };
 
-  });
-  //progress bar update
-  if(song.play()) {
-    setInterval(()=>{progress_bar.value = song.currentTime;}, 500);
-  }
- //progress bar functionality
-  progress_bar.addEventListener("change", function(){
-    song.play();
-    song.currentTime = progress_bar.value;
-    button_icon.classList.remove("fa-play");
-    button_icon.classList.add("fa-pause");
-  });
+  function play(num){
+    song[num].play();
+    button_icon[num].classList.remove("fa-play");
+    button_icon[num].classList.add("fa-pause");
+  };
 
-});
+  function pause(num){
+    song[num].pause();
+    button_icon[num].classList.remove("fa-pause");
+    button_icon[num].classList.add("fa-play");
+  };
 
-//under works
-document.addEventListener('DOMContentLoaded', function(event) {
+  function progress_change(num){
+    song[num].play();
+    song[num].currentTime = progress_bar[num].value;
+    button_icon[num].classList.remove("fa-play");
+    button_icon[num].classList.add("fa-pause");
+  };
 
-  let page = document.querySelector('p');
-  let body = document.querySelector('body');
-
-  page.addEventListener('click', function(event){
-    body.style.background = 'url(https://th.bing.com/th/id/R.737c8b5516687272221ac78311f0ac5b?rik=w9HJeTNtJd6YMw&pid=ImgRaw&r=0)';
-    body.style.backgroundSize = 'cover';
-  })
+  function change_bg(num){
+    bg.style.background = bgArray[num];
+    bg.style.backgroundSize = 'cover';
+  };
 
 });
